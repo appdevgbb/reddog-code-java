b9c9117e06c33002643b6c2efcddf3db517a8453
Govind Kamtamneni <govind.k@microsoft.com>
2022-09-08 16:54:36 -0700

    initial scaffold of makeline service; closes #13

---
+++ spring/makeline-service/src/main/java/com/microsoft/gbb/reddog/makelineservice/messaging/DaprPublisher.java
@@ -0,0 +1,26 @@
+package com.microsoft.gbb.reddog.makelineservice.messaging;
+
+import io.dapr.client.DaprClient;
+import io.dapr.client.DaprClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class DaprPublisher {
+
+    @Value("${messaging.pubsub.TOPIC_NAME}")
+    private String TOPIC_NAME;
+    @Value("${messaging.pubsub.SUB_NAME}")
+    private String SUB_NAME;
+
+    public void publishEvent(String message) {
+        DaprClient client = new DaprClientBuilder().build();
+        client.publishEvent(
+                SUB_NAME,
+                TOPIC_NAME,
+                message).block();
+    }
+
+}
+
